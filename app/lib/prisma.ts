67036c04de6785ccb56c17647cdbad3cf5002e49
AnshKr2004 <anshr795@gmail.com>
2024-09-25 18:59:48 +0530

    vercel deploy 6

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client'
 
-   declare global {
-     var prisma: PrismaClient | undefined
-   }
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
 
-   export const prisma = global.prisma || new PrismaClient()
+export const prisma = global.prisma || new PrismaClient()
 
-   if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma
+}
 
-   export default prisma
+export default prisma
