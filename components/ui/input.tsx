8cb7d82987e135a296fb1ce98dcd416b84ea15c9
HIMANSHU SHUKLA <120308745+raghavshulka@users.noreply.github.com>
2024-09-26 22:39:05 +0530

    check

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -3,7 +3,11 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+  type?: string;
+  props?: string;
+}
 
 const Input = React.forwardRef<
   HTMLInputElement,
